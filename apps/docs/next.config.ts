e816616c659729b744a5dda816bcd508265c9850
Sooraj Janardhanan <soorajkj46@gmail.com>
2025-09-07 23:59:31 +0530

    fix: :adhesive_bandage: docs default redirection

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -3,6 +3,15 @@ import { createMDX } from "fumadocs-mdx/next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs/get-started/introduction",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX();
