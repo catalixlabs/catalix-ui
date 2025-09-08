2b21870246bcefd296495305eaac6456b11aca82
Sooraj Janardhanan <soorajkj46@gmail.com>
2025-09-08 19:25:11 +0530

    feat: :sparkles: base landing page

---
+++ apps/docs/src/app/(www)/page.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Hero from "@/components/[www]/Hero";
+import Components from "@/components/[www]/Components";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Hero />
+      <Components />
+    </Fragment>
+  );
+}
