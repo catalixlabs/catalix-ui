4444cd2aa8cf88bd293c06d7abd1a3bdf9ca592f
Sooraj Janardhanan <soorajkj46@gmail.com>
2025-09-12 05:02:47 +0530

    refactor: :art: update registry schema, zod validators

---
+++ packages/cli/src/types/registry.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+const typeEnum = z.enum([
+  "registry:lib",
+  "registry:block",
+  "registry:component",
+  "registry:core",
+  "registry:hook",
+  "registry:page",
+  "registry:file",
+]);
+
+const fileSchema = z.object({
+  path: z.string(),
+  content: z.string().optional(),
+  type: typeEnum,
+  target: z.string().optional(),
+});
+
+export const registryItemSchema = z.object({
+  name: z.string(),
+  type: typeEnum,
+  title: z.string().optional(),
+  description: z.string().optional(),
+  author: z.string().optional(),
+  dependencies: z.array(z.string()).optional(),
+  devDependencies: z.array(z.string()).optional(),
+  registryDependencies: z.array(z.string()).optional(),
+  files: z.array(fileSchema),
+  docs: z.string().optional(),
+  categories: z.array(z.string()).optional(),
+});
+
+export const registrySchema = z.object({
+  name: z.string(),
+  homepage: z.string(),
+  items: z.array(registryItemSchema).min(1),
+});
+
+export type RegistryItem = z.infer<typeof registryItemSchema>;
+export type Registry = z.infer<typeof registrySchema>;
